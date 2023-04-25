db733d8994c2560a2094a38ac24602724f80cd0f
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-04-25 19:20:24 -0400

    Update 4_18_2023_Day_2.py

--- 4_18_2023_Day_2.py
+++ 4_18_2023_Day_2.py
@@ -1,35 +1,32 @@
 # Day 2 - Lists and Conditionals and Loops
 import sys # ignore for now
 
-# Take 3 different number inputs
-# Make a variable called total and set equal to 0
-
 # Lists
 my_list = ["test", 4, "e"]
 
-##print(my_list)
-##
-##my_list.append("Hi")
-##
-##print(my_list)
-##
-##my_list.append(1)
-##
-##print(my_list)
-##print(len(my_list))
-##
-##my_list.remove("Hi")
-##
-##print(my_list)
-##print(len(my_list))
-##
-##my_list.remove(1)
+print(my_list)
 
-##print(my_list[0])
+my_list.append("Hi")
 
-##my_list.insert(1, "blah")
-##
-##print(my_list)
+print(my_list)
+
+my_list.append(1)
+
+print(my_list)
+print(len(my_list))
+
+my_list.remove("Hi")
+
+print(my_list)
+print(len(my_list))
+
+my_list.remove(1)
+
+print(my_list[0])
+
+my_list.insert(1, "blah")
+
+print(my_list)
 
 my_list[1] = 2
 
