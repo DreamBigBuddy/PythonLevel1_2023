1270fbb918a22207c4a4b7cf5baab212f9959646
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-04-25 19:20:08 -0400

    Update 4_4_2023_Day_1.py

--- 4_4_2023_Day_1.py
+++ 4_4_2023_Day_1.py
@@ -1,22 +1,17 @@
-##my_int = 1
-##my_str = "Hello"
-##
-##print(my_int)
-##print(my_str)
+my_int = 1
+my_str = "Hello"
+
+print(my_int)
+print(my_str)
 
 my_input = int(input("Enter a number: "))
 my_input2 = int(input("Enter another number: "))
 print(my_input + my_input2)
 
-##my_input = int(input("Enter another number: "))
-##print(my_input + 10)
+my_input = int(input("Enter another number: "))
+print(my_input + 10)
 
-##my_int = 1
-##
-##my_int2 = my_int * 2
-##print(my_int2)
+my_int = 1
 
-# Exercise:
-# Get two number inputs from the user
-# Do any math operation to the two numbers (+, - , *, /)
-# Return that result to the user
+my_int2 = my_int * 2
+print(my_int2)
