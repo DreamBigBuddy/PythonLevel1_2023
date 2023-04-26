bfcf30c4ba096544b8e3af64e90c80ba5cf0721e
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-04-25 20:27:43 -0400

    Update and rename 4_18_2023_Day_2.py to 4_18_2023_Lists.py

---
+++ 4_18_2023_Lists.py
@@ -0,0 +1,33 @@
+# Day 2 - Lists and Conditionals and Loops
+import sys # ignore for now
+
+# Lists
+my_list = ["test", 4, "e"]
+
+print(my_list)
+
+my_list.append("Hi")
+
+print(my_list)
+
+my_list.append(1)
+
+print(my_list)
+print(len(my_list))
+
+my_list.remove("Hi")
+
+print(my_list)
+print(len(my_list))
+
+my_list.remove(1)
+
+print(my_list[0])
+
+my_list.insert(1, "blah")
+
+print(my_list)
+
+my_list[1] = 2
+
+print(my_list)
