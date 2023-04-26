2b325a0e847636bacf58519830f2d1bf5118890a
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-04-25 20:29:21 -0400

    Create 4_25_2023_ForLoop.py

---
+++ 4_25_2023_ForLoop.py
@@ -0,0 +1,15 @@
+# For Loop
+for i in range(0, 10, 1): # Change values to see how a for loop can be modified
+    print(i)
+
+# List and Range
+my_list = list(range(10))
+print(my_list)
+
+# More For Loops
+# Summing a range of numbers
+total = 0
+for i in range(10):
+    total += i
+
+print(total)
