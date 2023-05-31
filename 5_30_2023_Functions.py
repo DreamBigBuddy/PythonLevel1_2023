cfe55e233ab3f71c8f3a9d64140d434ad1dab6e4
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-05-30 20:25:10 -0400

    Add files via upload

---
+++ 5_30_2023_Functions.py
@@ -0,0 +1,24 @@
+# Functions
+
+import turtle
+
+t = turtle.Turtle()
+
+##def printName():
+##    print("Hi Vaman!")
+##
+##printName()
+
+##def add(x, y):
+##    print(x + y)
+##
+##add(5, 10)
+
+def drawSquare(side_length):
+    for i in range(4):
+        t.forward(side_length)
+        t.right(90)
+
+drawSquare(100)
+
+t.circle(50, 180)
