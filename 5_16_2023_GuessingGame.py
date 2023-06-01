d61d8d65b9f940d27a6266f79141a04a663e8cbc
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2023-06-01 18:01:56 -0400

    Update 5_16_2023_GuessingGame.py

--- 5_16_2023_GuessingGame.py
+++ 5_16_2023_GuessingGame.py
@@ -1,13 +1,17 @@
-# More While Loops
+# From Dhanshika
 
 import random
 
-answer = random.randint(1, 100)
+answer = random.randint(1,100)
 
-guess = int(input("Enter a guess: "))
+guess = int(input("Enter any Number:"))
+    
+while answer != guess:
+    if answer<guess:
+        print("Number too high! \U0001F446")
+    elif answer>guess: 
+        print("Number too low! \U0001F447")        
 
-while guess != answer:
-    print("Incorrect!")
-    guess = int(input("Enter a guess: "))
+    guess = int(input("Enter a Guess"))
 
-print("Correct!")
+print("Good Job! \U0001f600")
